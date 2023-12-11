2bf42349aa49b32395a27d6fcb41d393efbd8730
Priyanshu Kuldeep <itzchaturvedi@gmail.com>
2023-12-11 23:28:59 +0530

    Add project files.

---
+++ E-commerce Product Management API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace E_commerce_Product_Management_API.Models
+{
+    public class Product
+    {
+        //Product model with properties such as ProductID, Name, Description, Price, StockQuantity, and Category
+        public int ProductID { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ E-commerce Product Management API/Models/ProductContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce_Product_Management_API.Models
+{
+    public class ProductContext :DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+        public DbSet<Product> Product { get; set; } = null!;
+    }
+}
